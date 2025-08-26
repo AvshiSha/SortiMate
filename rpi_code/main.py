123fac973b0e93d9decdf65b0b7ea420cfaa7539
Rotem Waingart <Rotem.waingart@gmail.com>
2025-08-26 12:35:30 +0300

    angles testing

--- rpi_code/main.py
+++ rpi_code/main.py
@@ -1,3 +1,42 @@
+import time
+from sorting_mechanism import SortingMechanism, WasteType
+
+def main():
+    try:
+        sorter = SortingMechanism(rotation_pin=17, gate_pin=27) 
+        print("Smart Recycling Bin initialized...")
+        
+        # time.sleep(2)
+        # sorter.gate_servo.set_angle(150)
+        # time.sleep(2)
+        # sorter.rotation_servo.set_angle(180)
+        # time.sleep(2)
+        # sorter.rotation_servo.set_angle(0)
+        # time.sleep(2)
+        
+        print("moving gate to 90")
+        sorter.gate_servo.set_angle(90)
+        time.sleep(2)
+        print("moving gate to 0")
+        sorter.gate_servo.set_angle(0)
+        time.sleep(2)
+        print("moving gate to 90")
+        sorter.gate_servo.set_angle(90)
+        time.sleep(2)
+        
+        
+    except KeyboardInterrupt:
+        print("\nProgram interrupted by user")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        # Clean up GPIO resources
+        sorter.cleanup()
+
+if __name__ == "__main__":
+    main()
+
+
 # import time
 # import os
 # from sorting_mechanism import SortingMechanism, WasteType
